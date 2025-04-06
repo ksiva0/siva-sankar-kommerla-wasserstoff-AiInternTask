9b383cbc5af4e6b55210b07e74d6b3a6bd4e05bf
Siva Sankar Kommerla <147400354+ksiva0@users.noreply.github.com>
2025-04-06 12:49:23 +0530

    Update gmail_service.py

--- Email_Assisstant/src/services/gmail_service.py
+++ Email_Assisstant/src/services/gmail_service.py
@@ -33,18 +33,17 @@ class GmailService:
                     try:
                         flow = Flow.from_client_config(
                             {
-                                "web": {
+                                "web": {  # Changed from root to web. This is CRUCIAL
                                     "client_id": st.secrets["google_oauth"]["client_id"],
                                     "client_secret": st.secrets["google_oauth"]["client_secret"],
-                                    "redirect_uris": [st.secrets["google_oauth"]["redirect_uri"]],
-                                    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                                    "redirect_uris": [st.secrets["google_oauth"]["redirect_uri"]],  # Changed to a list
+                                    "auth_uri": "https://accounts.google.com/o/oauth2/auth",  # added these as they were in the original code
                                     "token_uri": "https://oauth2.googleapis.com/token"
                                 }
                             },
-                            scopes=['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.send'],  # Added send scope
+                            scopes=['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.send'],
                             redirect_uri=st.secrets["google_oauth"]["redirect_uri"]
                         )
-                        
                         flow.fetch_token(code=code)
                         creds = flow.credentials
                         st.session_state['token'] = creds.to_json()  # store token in session state.
@@ -56,18 +55,17 @@ class GmailService:
                     try:
                         flow = Flow.from_client_config(
                             {
-                                "web": {
+                                "web": {  # Changed from root to web. This is CRUCIAL
                                     "client_id": st.secrets["google_oauth"]["client_id"],
                                     "client_secret": st.secrets["google_oauth"]["client_secret"],
-                                    "redirect_uris": [st.secrets["google_oauth"]["redirect_uri"]],
-                                    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                                    "redirect_uris": [st.secrets["google_oauth"]["redirect_uri"]],  # Changed to a list
+                                    "auth_uri": "https://accounts.google.com/o/oauth2/auth",  # added these as they were in the original code
                                     "token_uri": "https://oauth2.googleapis.com/token"
                                 }
                             },
-                            scopes=['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.send'],  # Added send scope
+                            scopes=['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.send'],
                             redirect_uri=st.secrets["google_oauth"]["redirect_uri"]
                         )
-                        
                         authorization_url, state = flow.authorization_url(
                             access_type='offline',
                             include_granted_scopes='true'
