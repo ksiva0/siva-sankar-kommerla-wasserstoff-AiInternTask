b1d283a95b4f9402a485e2479ced18c26d759f36
Siva Sankar Kommerla <147400354+ksiva0@users.noreply.github.com>
2025-04-07 18:27:25 +0530

    Update email_controller.py

--- Email_Assisstant/src/controllers/email_controller.py
+++ Email_Assisstant/src/controllers/email_controller.py
@@ -1,6 +1,8 @@
 # Email_Assistant/src/controllers/email_controller.py
 
 from fastapi import APIRouter
+from services.gmail_service import GmailService
+from utils.prompt_engineering import generate_prompt
 
 router = APIRouter()
 
